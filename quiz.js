ca1160aa45c559d6463c4a335732a4b40226675a
j-west <westja89@gmail.com>
2016-12-09 17:27:24 -0600

    Add for loop inside populatePage function to loop over the data.cars array.  The loop creates two divs, one for a row, and one for the column.  The colDiv gets a col-md-3 and col-md-offset-1 classes, then gets several heading and p elements set as it's innerHTML. The colDiv then gets appended to the rowDiv which gets appended to the mainDiv

--- quiz.js
+++ quiz.js
@@ -6,6 +6,28 @@ var mainDiv = document.querySelector(".container")
 
 
 function populatePage(data) {
+  for (var i = 0; i < data.cars.length; i++) {
+    var rowDiv = document.createElement("div");
+    var colDiv1 = document.createElement("div");
+    // var colDiv2 = document.createElement("div");
+    // var colDiv3 = document.createElement("div");
+    colDiv1.classList.add("col-md-3")
+    if (i !== 0) {
+      colDiv1.classList.add("col-md-offset-1")
+    }
+    colDiv1.innerHTML =  `
+                          <h2 class="text-center">${data.cars[i].make}</h2>
+                          <h3 class="text-center">${data.cars[i].model}</h3>
+                          <p class="text-center">${data.cars[i].year}</p>
+                          <p class="text-center">${data.cars[i].price}</p>
+                          <p>${data.cars[i].description}</p>
+                        `
+
+console.log(colDiv1);
+    rowDiv.appendChild(colDiv1);
+    mainDiv.appendChild(rowDiv)
+    // rowDiv.appendChild(colDiv2);
+    // rowDiv.appendChild(colDiv3);
 
   }
 }
