e2e14539f240d612dd609194072e108b91954650
j-west <westja89@gmail.com>
2016-12-09 13:41:10 -0600

    Added loadInventory function to create an XHR and parse the responseText into a variable named data.  Then created populatePage function

---
+++ quiz.js
@@ -0,0 +1,18 @@
+
+function populatePade(data) {
+
+}
+
+function loadInventory() {
+  var myRequest = new XMLHttpRequest();
+
+  myRequest.addEventListener("load", (e) => {
+    var data = JSON.parse(e.target.responseText)
+    console.log(data);
+
+  });
+  myRequest.open("GET", "inventory.json")
+  myRequest.send();
+}
+
+loadInventory();
