f12f1894c5257b32f53850ab180185c231eeebd7
j-west <westja89@gmail.com>
2016-12-09 18:36:14 -0600

    Move variable declaration to outside for loop so only one row div is created.  Will update to add another rowDiv after three colDivs have been created in the loop when I finish the rest of the basic functionality

--- quiz.js
+++ quiz.js
@@ -57,27 +57,27 @@ function activateEvents() {
 }
 
 function populatePage(data) {
-      var rowDiv = document.createElement("div");
-          rowDiv.classList.add("row")
+  var rowDiv = document.createElement("div");
+  rowDiv.classList.add("row")
+
   for (var i = 0; i < data.cars.length; i++) {
     var colDiv1 = document.createElement("div");
-    colDiv1.classList.add("col-md-3", "border-class")
+    colDiv1.classList.add("col-md-3", "border-class", "target-class")
+
     if (i !== 0) {
       colDiv1.classList.add("col-md-offset-1")
     }
     colDiv1.innerHTML =  `
-                          <h2 class="text-center">${data.cars[i].make}</h2>
-                          <h3 class="text-center">${data.cars[i].model}</h3>
-                          <p class="text-center">${data.cars[i].year}</p>
-                          <p class="text-center">${data.cars[i].price}</p>
-                          <p>${data.cars[i].description}</p>
+                          <h2 class="text-center target-class">${data.cars[i].make}</h2>
+                          <h3 class="text-center target-class">${data.cars[i].model}</h3>
+                          <p class="text-center target-class">${data.cars[i].year}</p>
+                          <p class="text-center target-class">${data.cars[i].price}</p>
+                          <p class="target-class">${data.cars[i].description}</p>
                         `
 
-console.log(colDiv1);
+    console.log(colDiv1);
     rowDiv.appendChild(colDiv1);
     mainDiv.appendChild(rowDiv)
-
-
   }
 }
 
@@ -95,3 +95,4 @@ function loadInventory() {
 }
 
 loadInventory();
+activateEvents();
